471b25bae01b475419aaca6d4125a835cd09ea8d
Chento <chento271095@gmail.com>
2026-06-08 21:32:29 -0500

    archivos de cambios

--- app/Http/Middleware/ForceCurrentUrl.php
+++ app/Http/Middleware/ForceCurrentUrl.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
 
 class ForceCurrentUrl
@@ -14,6 +15,28 @@ class ForceCurrentUrl
         URL::forceRootUrl($request->getSchemeAndHttpHost());
         URL::forceScheme($request->getScheme());
 
+        config([
+            'session.domain' => $this->sessionDomainFor($request),
+            'session.secure' => $request->isSecure(),
+            'session.same_site' => 'lax',
+            'session.path' => '/',
+        ]);
+
         return $next($request);
+    }
+
+    private function sessionDomainFor(Request $request): ?string
+    {
+        $host = strtolower($request->getHost());
+
+        if ($host === 'localhost' || preg_match('/^\d{1,3}(\.\d{1,3}){3}$/', $host)) {
+            return null;
+        }
+
+        if (Str::startsWith($host, 'www.')) {
+            $host = Str::after($host, 'www.');
+        }
+
+        return '.'.$host;
     }
 }
