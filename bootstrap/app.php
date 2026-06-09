81a235e970f5acc3d8604a042a988273a4130107
Chento <chento271095@gmail.com>
2026-06-08 21:25:56 -0500

    archivos de cambios

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use App\Http\Middleware\ForceCurrentUrl;
 use App\Http\Middleware\EnsureUserIsAdministrator;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -15,6 +16,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'admin' => EnsureUserIsAdministrator::class,
+        ]);
+
+        $middleware->trustProxies(at: '*');
+        $middleware->web(prepend: [
+            ForceCurrentUrl::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
